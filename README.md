80be650c46e410b05604ca50f9610d12fdf846f8
jaanus <zaar2213@gmail.com>
2024-12-11 01:06:39 +0200

    frontend game box adjustments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # API Game
+
+## frontend
+
+```python
+cd frontend
+npm install
+npm run dev
+```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-
-import styles from './App.module.css'
+import { useState, useEffect } from 'react';
+import styles from './App.module.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
 
+  useEffect(() => {
+    if (!loggedIn) {
+      fetch('localhost:8080/connection');
+    }
+  });
   return (
     <div className={styles.container}>
-      <span>API GAME</span>
+      <span className={styles.title}>API GAME</span>
+      {loggedIn && <div className={styles.gameWindow}></div>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
