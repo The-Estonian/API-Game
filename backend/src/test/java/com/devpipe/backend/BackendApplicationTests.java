7cb9493dbdc1c6bf131da28342d6103ebbe2ed74
jaanus <zaar2213@gmail.com>
2024-12-11 01:06:17 +0200

    added Java spring boot backend

---
+++ backend/src/test/java/com/devpipe/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpipe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
